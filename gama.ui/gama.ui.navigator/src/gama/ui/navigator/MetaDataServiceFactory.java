f966f26595c7d3ff2d20c3aac910dc60493294f2
AlexisDrogoul <alexis.drogoul@gmail.com>
2023-05-05 22:02:37 +0700

    Enables the creation and compilation of GAML files.

--- gama.ui/gama.ui.navigator/src/gama/ui/navigator/MetaDataServiceFactory.java
+++ gama.ui/gama.ui.navigator/src/gama/ui/navigator/MetaDataServiceFactory.java
@@ -1,12 +1,12 @@
 /*******************************************************************************************************
  *
- * MetaDataServiceFactory.java, in gama.ui.navigator, is part of the source code of the
- * GAMA modeling and simulation platform (v.1.9.0).
+ * MetaDataServiceFactory.java, in gama.ui.navigator, is part of the source code of the GAMA modeling and simulation
+ * platform (v.1.9.2).
  *
- * (c) 2007-2022 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
+ * (c) 2007-2023 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
- * 
+ *
  ********************************************************************************************************/
 package gama.ui.navigator;
 
@@ -24,14 +24,12 @@ public class MetaDataServiceFactory extends AbstractServiceFactory {
 	/**
 	 * Instantiates a new meta data service factory.
 	 */
-	public MetaDataServiceFactory() {
-	}
+	public MetaDataServiceFactory() {}
 
 	@Override
-	public Object create(final Class serviceInterface, final IServiceLocator parentLocator,
+	public IFileMetaDataProvider create(final Class serviceInterface, final IServiceLocator parentLocator,
 			final IServiceLocator locator) {
-		if (IFileMetaDataProvider.class.equals(serviceInterface))
-			return FileMetaDataProvider.getInstance();
+		if (IFileMetaDataProvider.class.equals(serviceInterface)) return FileMetaDataProvider.getInstance();
 		return null;
 	}
 
